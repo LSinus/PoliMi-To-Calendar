883dc6694d21066aa7ce96e6bb9cc3304296eeb5
TreOndeDiCani <leonardosinibaldi21@gmail.com>
2023-10-21 20:46:36 +0200

    webpack fix

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,6 +4,7 @@ const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common.js');
 const PATHS = require('./paths');
+const Dotenv = require('dotenv-webpack');
 
 // Merge webpack configuration files
 const config = (env, argv) =>
@@ -14,6 +15,9 @@ const config = (env, argv) =>
       background: PATHS.src + '/background.js',
     },
     devtool: argv.mode === 'production' ? false : 'source-map',
+    plugins: [
+      new Dotenv(),
+    ],
   });
 
 module.exports = config;
